78e510cf41d6b3c74b76dff7ad5de686bf335a2c
andrey <andrey00989@gmail.com>
2023-02-16 16:54:28 +0300

    homework my-dogs v1

---
+++ src/components/SearchInfo/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const SearchInfo = ({ searchText, searchCount }) => {
+  return (
+    searchText && (
+      <section className="search-title">
+        По запросу <span>{searchText}</span> найдено {searchCount} товаров
+      </section>
+    )
+  );
+};
+
+export default SearchInfo;
